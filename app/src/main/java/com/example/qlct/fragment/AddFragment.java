d71ac618e7af1042b553a85718dcf664b44701c6
congphamtienthanh <congphamtienthanh@gmail.com>
2022-06-12 23:59:19 +0700

    edit code

--- app/src/main/java/com/example/qlct/fragment/AddFragment.java
+++ app/src/main/java/com/example/qlct/fragment/AddFragment.java
@@ -8,21 +8,14 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import com.example.qlct.R;
-import com.example.qlct.activity.MainActivity;
 import com.example.qlct.adapter.AddAdapter;
-import com.example.qlct.adapter.PeriodicAdapter;
 import com.example.qlct.dialog.AddDialog;
-import com.example.qlct.message.MessageReceiver;
 import com.example.qlct.model.Item;
-import com.example.qlct.model.Mess;
 import com.example.qlct.realm.RealmController;
 
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import butterknife.BindView;
@@ -105,73 +98,4 @@ public class AddFragment extends Fragment {
         addDialog.show(getActivity().getSupportFragmentManager(), "dialog_add");
         addDialog.setCancelable(false);
     }
-
-//    int type;
-//
-//    public void receiveMess(String sender, String body) {
-//        String bank = sender;
-//        String amount = getAmount(body);
-//        Mess message = new Mess(bank, type, amount);
-//
-//        addFromBank(message);
-//    }
-//
-//    private String getBank(String sender) {
-//        if (MessageReceiver.LIST_BANK.contains(sender)) {
-//            return sender;
-//        }
-//        return null;
-//    }
-//
-//    private String getAmount(String bodyMess) {
-////        String a = bodyMess.split("VND")[0];
-//        String string2 = "";
-//        String amount = "";
-//        if (bodyMess != null) {
-//            if (bodyMess.contains("+")) {
-//                type = 1;
-//                string2 = bodyMess.split("\\+")[1];
-//
-//                for (int i = 0; i < string2.length(); i++) {
-//                    try {
-//                        Integer.parseInt(string2.charAt(i) + "");
-//                        amount = amount + "" + string2.charAt(i);
-//                    } catch (NumberFormatException e) {
-//                        e.printStackTrace();
-//                        if (!(string2.charAt(i)+"").equals(" ") && !(string2.charAt(i)+"").equals(",") && !(string2.charAt(i)+"").equals(".")){
-//                            break;
-//                        }
-//                    }
-//                }
-//            } else if (bodyMess.contains("-")) {
-//                type = 2;
-//                string2 = bodyMess.split("-")[1];
-//                for (int i = 0; i < string2.length(); i++) {
-//                    try {
-//                        Integer.parseInt(string2.charAt(i) + "");
-//                        amount = amount + "" + string2.charAt(i);
-//                    } catch (NumberFormatException e) {
-//                        e.printStackTrace();
-//                        if (!(string2.charAt(i)+"").equals(" ") && !(string2.charAt(i)+"").equals(",") && !(string2.charAt(i)+"").equals(".")){
-//                            break;
-//                        }
-//                    }
-//                }
-//            }
-//            return amount;
-//        }
-//        return null;
-//    }
-//
-//    public void addFromBank(Mess mess) {
-//        Date date = new Date();
-//        String strDateFormat = "dd/MM/yyyy";
-//        SimpleDateFormat sdf = new SimpleDateFormat(strDateFormat);
-//        String d = sdf.format(date);
-//
-//        Item item = new Item((int) System.currentTimeMillis(), mess.type, mess.bank, "Ngân hàng", d, null, Long.parseLong(mess.amount), null, AddDialog.getMonth(d), AddDialog.getYear(d));
-//        realmController.addItem(item);
-//        addList = realmController.getItem(1);
-//        addAdapter.notifyDataSetChanged();
-//    }
 }
--- app/src/main/java/com/example/qlct/message/MessageListener.java
+++
@@ -1,9 +0,0 @@
-package com.example.qlct.message;
-
-import com.example.qlct.model.Mess;
-
-public interface MessageListener {
-
-//    void messageReceived(Mess message);
-    void messageReceived(String sender, String body);
-}
